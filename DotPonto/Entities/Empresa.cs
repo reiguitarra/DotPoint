d91a0bf3d4229ee59d7ec62aa8097abe9d3c466d
Reinaldo Avelino <reiguitarra@gmail.com>
2020-08-25 11:59:13 -0300

    Foi criado as primeiras entidades

---
+++ DotPonto/Entities/Empresa.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotPonto.Entities
+{
+    public class Empresa
+    {
+        public int Id { get; set; }
+        public string RazaoSocial { get; set; }
+        public string EmpEndereco { get; set; }
+        public string EmpEndNumero { get; set; }
+        public string EmpEndBairro { get; set; }
+        public string EmpEndCidade { get; set; }
+        public string EmpEndUF { get; set; }
+        public string EmpEndCEP { get; set; }
+        public string CNAE { get; set; }
+        public string InscricaoMunicipal { get; set; }
+        public string InscricaoEstadual { get; set; }
+        public ICollection<Filial> Filial { get; set; }
+
+
+
+        public Empresa()
+        {
+
+        }
+
+        public Empresa(int id, string razaoSocial, string empEndereco, string empEndNumero, 
+            string empEndBairro, string empEndCidade, string empEndUF, string empEndCEP, 
+            string cNAE, string inscricaoMunicipal, string inscricaoEstadual)
+        {
+            Id = id;
+            RazaoSocial = razaoSocial;
+            EmpEndereco = empEndereco;
+            EmpEndNumero = empEndNumero;
+            EmpEndBairro = empEndBairro;
+            EmpEndCidade = empEndCidade;
+            EmpEndUF = empEndUF;
+            EmpEndCEP = empEndCEP;
+            CNAE = cNAE;
+            InscricaoMunicipal = inscricaoMunicipal;
+            InscricaoEstadual = inscricaoEstadual;
+        }
+    }
+}
---
+++ DotPonto/Entities/Filial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotPonto.Entities
+{
+    public class Filial : Empresa
+    {
+        public Filial(int id, string razaoSocial, string empEndereco, string empEndNumero,
+            string empEndBairro, string empEndCidade, string empEndUF, string empEndCEP,
+            string cNAE, string inscricaoMunicipal, string inscricaoEstadual) 
+            : base(id, razaoSocial, empEndereco, empEndNumero, 
+                  empEndBairro, empEndCidade,empEndUF,empEndCEP, cNAE, inscricaoMunicipal,inscricaoEstadual)
+        {
+
+        }
+
+    }
+}
